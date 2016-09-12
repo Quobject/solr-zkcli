22378e9383bee7b48fd36debc26acb8b41b21b52
MatthiasL <matthias_ludwig@yahoo.com>
2016-09-13 05:09:19 +1000

    readded Dockerfile

---
+++ dockerfile/docker-entrypoint.sh
@@ -0,0 +1,12 @@
+﻿#!/bin/bash
+set -e
+
+#echo $1
+
+if [ "$1" = '/opt/solr/bin/solr' ]; then
+	chown -R solr .
+  sync # got error "error: exec failed: text file busy". use this as suggested at https://github.com/docker/docker/issues/9547
+	exec gosu solr "$@"
+fi
+
+exec "$@"
